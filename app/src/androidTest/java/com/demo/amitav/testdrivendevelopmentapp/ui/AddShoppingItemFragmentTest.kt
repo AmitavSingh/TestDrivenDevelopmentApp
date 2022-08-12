49867f005539a4dc9f751418d0a490b749f8b4ad
amitav.singh <singh.amitav26@gmail.com>
2022-08-12 12:29:36 +0200

    Testing Addition of Shopping Items

--- app/src/androidTest/java/com/demo/amitav/testdrivendevelopmentapp/ui/AddShoppingItemFragmentTest.kt
+++ app/src/androidTest/java/com/demo/amitav/testdrivendevelopmentapp/ui/AddShoppingItemFragmentTest.kt
@@ -1,14 +1,20 @@
 package com.demo.amitav.testdrivendevelopmentapp.ui
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.pressBack
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.filters.MediumTest
 import com.demo.amitav.testdrivendevelopmentapp.R
+import com.demo.amitav.testdrivendevelopmentapp.data.local.ShoppingItem
+import com.demo.amitav.testdrivendevelopmentapp.getOrAwaitValue
 import com.demo.amitav.testdrivendevelopmentapp.launchFragmentInHiltContainer
+import com.demo.amitav.testdrivendevelopmentapp.repositories.FakeShoppingRepositoryAndroidTest
+import com.google.common.truth.Truth.assertThat
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -18,6 +24,7 @@ import org.junit.Test
 import org.mockito.Mock
 import org.mockito.Mockito.verify
 import org.mockito.MockitoAnnotations
+import javax.inject.Inject
 
 @MediumTest
 @HiltAndroidTest
@@ -30,28 +37,60 @@ class AddShoppingItemFragmentTest {
     @Mock
     lateinit var navController: NavController
 
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Inject
+    lateinit var fragmentFactory: ShoppingFragmentFactory
+
+    private lateinit var testViewModel: ShoppingViewModel
+
     @Before
     fun setup() {
         hiltRule.inject()
         MockitoAnnotations.initMocks(this)
-
-        launchFragmentInHiltContainer<AddShoppingItemFragment> {
-            Navigation.setViewNavController(requireView(), navController)
-        }
+        testViewModel = ShoppingViewModel(FakeShoppingRepositoryAndroidTest())
     }
 
     @Test
     fun pressBackButton_popBackStack() {
+        launchFragmentInHiltContainer<AddShoppingItemFragment>(
+            fragmentFactory = fragmentFactory
+        ) {
+            Navigation.setViewNavController(requireView(), navController)
+        }
         pressBack()
         verify(navController).popBackStack()
     }
 
     @Test
     fun clickAddImageItemButton_navigateToImagePickFragment() {
+        launchFragmentInHiltContainer<AddShoppingItemFragment>(
+            fragmentFactory = fragmentFactory
+        ) {
+            Navigation.setViewNavController(requireView(), navController)
+        }
         Espresso.onView(ViewMatchers.withId(R.id.ivShoppingImage)).perform(ViewActions.click())
         verify(navController).navigate(
             AddShoppingItemFragmentDirections.actionAddShoppingItemFragmentToImagePickFragment()
         )
+    }
+
+    @Test
+    fun clickInsertIntoDb_shoppingItemInsertedIntoDb() {
+        launchFragmentInHiltContainer<AddShoppingItemFragment>(
+            fragmentFactory = fragmentFactory
+        ) {
+            viewModel = testViewModel
+        }
+        Espresso.onView(withId(R.id.etShoppingItemName))
+            .perform(ViewActions.replaceText("shopping item"))
+        Espresso.onView(withId(R.id.etShoppingItemAmount)).perform(ViewActions.replaceText("5"))
+        Espresso.onView(withId(R.id.etShoppingItemPrice)).perform(ViewActions.replaceText("5.5"))
+        Espresso.onView(withId(R.id.btnAddShoppingItem)).perform(ViewActions.click())
+
+        assertThat(testViewModel.shoppingItems.getOrAwaitValue())
+            .contains(ShoppingItem("shopping item", 5, 5.5f, ""))
     }
 }
 
